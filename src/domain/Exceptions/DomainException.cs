b93efc11c6d1737017cb7f4c0392957f40f51e8b
Eduardo Gomes <edugomesti@outlook.com>
2023-06-09 10:01:19 -0300

    feat: criado domain exeptions

---
+++ src/domain/Exceptions/DomainException.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+
+namespace Domain.Exceptions;
+
+[Serializable]
+public abstract class DomainException : Exception
+{
+    protected DomainException(string message) : base(message)
+    {
+    }
+
+    protected DomainException(SerializationInfo info, StreamingContext content) : base(info, content)
+    {
+    }
+}
